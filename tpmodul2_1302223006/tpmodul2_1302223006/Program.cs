d8543bbf5f94b88b577510f5d9582f1c7ca7b9e0
sorayahaidars <sorayahaidars14@gmail.com>
2024-03-03 23:32:18 +0700

    commit awal

---
+++ tpmodul2_1302223006/tpmodul2_1302223006/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Masukkan satu huruf: ");
+string huruf = Console.ReadLine();
+
+if (huruf == "A" || huruf == "I" || huruf == "U" || huruf == "E" || huruf == "O") 
+{
+    Console.WriteLine("Huruf " + huruf + " merupakan huruf vokal");
+}
+else
+{
+    Console.WriteLine("Huruf " + huruf + " merupakan huruf konsonan");
+}
+
+int[] angka = { 2, 4, 6, 8, 10 };
+
+for (int i = 0; i < angka.Length; i++)
+{
+    Console.WriteLine("Angka genap " + (i + 1) + " :" + angka[i]);
+}
